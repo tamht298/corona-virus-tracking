ada8d623a9fa6b0fc695a2ecf659d2271c82dd26
Thanh Tam <tamht298@gmail.com>
2020-06-05 10:30:23 +0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dz-corona-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'corona-virus';
+}
